3509846796ef81fc1fb2385537a901d1931d6c85
Kyeongmin <147589193+mindyhere@users.noreply.github.com>
2026-04-16 21:54:22 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.50 ms, Memory: 72.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(int n) {
+        StringBuilder sb = new StringBuilder();
+        int cnt = n/2;
+        
+        for (int i=0; i < cnt; i++) {
+            sb.append("수박");
+        }
+        
+        if (n % 2 != 0) {
+            sb.append("수");
+        }
+
+        return sb.toString();
+    }
+}
